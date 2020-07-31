9e5cb4e455e8039c37f17bf33792588c25b60e34
Lukas Pieper <30661176+lukaspieper@users.noreply.github.com>
2020-07-31 19:29:35 +0200

    Add parameter for DotCover filter (excluding xunit.assert by default)

--- build/Build.Configuration.cs
+++ build/Build.Configuration.cs
@@ -5,6 +5,9 @@ partial class Build
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    [Parameter("DotCover coverage filter (default: '+:module=*;class=*;function=*;-:module=xunit.assert;')")]
+    readonly string DotCoverFilter = "+:module=*;class=*;function=*;-:module=xunit.assert;";
+
     [Parameter("Minimal value for CodeMetrics' MaintainabilityIndex. If a member is rated lower, the process fails")]
     readonly int MaintainabilityIndexMinimum = 60;
 }
