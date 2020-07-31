faa2fbfdbeb577d16c8c57bd7dd1fd8a974b6f8c
Lukas Pieper <30661176+lukaspieper@users.noreply.github.com>
2020-07-31 20:13:39 +0200

    Implement Prism

--- src/Freedirect.Application/App.xaml.cs
+++ src/Freedirect.Application/App.xaml.cs
@@ -1,6 +1,18 @@
-﻿namespace Freedirect.Application
+﻿using System.Windows;
+using Freedirect.Application.View;
+using Prism.Ioc;
+
+namespace Freedirect.Application
 {
     public partial class App
     {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
     }
 }
